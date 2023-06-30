3399648c48e5201ce14840622b933f195044bd0f
renemichelnunes <rene.michel@gmail.com>
2023-06-30 13:01:19 -0300

    Add files via upload
    dasd

--- Text_editor.ino
+++ Text_editor.ino
@@ -28,12 +28,29 @@
 TFT_eSPI tft = TFT_eSPI();                   // Invoke custom library with default width and height
 
 class Text{
-  // Char
+  // Char is 10x8 on size 2
   uint8_t font_size = 2;
   String text = "1234567890";
   uint8_t lines = 1;
   uint8_t char_per_lines = 10; 
+  uint8_t x = 0;
+  uint8_t y = 0;
 };
+
+void randChar(){
+  uint16_t x = 0, y = 0, color = 0;
+  uint8_t a = 0;
+  // this causes a overflow and a infinite loop
+  for(uint16_t i = 0; i < 65536; i++){
+    x = random(480);
+    y = random(320);
+    a = random(128);
+    color = random(65536);
+    //tft.drawChar(' ', x, y, 2);
+    tft.setTextColor(color);
+    tft.drawChar(char(a), x, y, 2);
+  }
+}
 
 void setup()
 {
@@ -48,6 +65,13 @@ void setup()
   tft.fillScreen(TFT_BLACK);
   tft.drawString("ooooooooooooooooooooooooooooo oooooooooooooooooooooooooooooa", 0, 0, 2);
   tft.drawString("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO OOOOOOOOOOOOOOOOOOOOOA", 0, 12, 2);
+  String a = "test";
+  a += 'e';
+  tft.drawString(a, 0, 24, 2);
+  a.remove(a.length()-1);
+  tft.drawString(a, 0, 36, 2);
+  delay(2000);
+  randChar();
 }
 
 void loop()
